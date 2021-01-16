2ae438bf2e089760944d15a5cefd393bb85297e9
Blobtoe <felixxperron@gmail.com>
2021-01-15 18:07:58 -0800

    added basic flask

--- start.py
+++ start.py
@@ -3,12 +3,21 @@
 run flask server and start the scheduler
 
 '''
-
+from flask import Flask, request, abort, render_template
 import time
 
 # local imports
 from PassScheduler import PassScheduler
 
+app = Flask(__name__)
 scheduler = PassScheduler()
-scheduler.start()
-time.sleep(10000)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html", data=scheduler.passes)
+
+
+if __name__ == "__main__":
+    scheduler.start()
+    app.run(port=80, host="0.0.0.0")
