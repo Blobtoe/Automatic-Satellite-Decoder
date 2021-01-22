44a0ec288adbeaedc6c4787f3a6f52d0065be224
Blobtoe <felixxperron@gmail.com>
2021-01-21 17:05:22 -0800

    added after parameter to next pass flask endpoint

--- start.py
+++ start.py
@@ -3,19 +3,27 @@
 run flask server and start the scheduler
 
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 # local imports
 from PassScheduler import PassScheduler
+import utils
 
 app = Flask(__name__)
 scheduler = PassScheduler()
 
 
 @app.route('/next/pass', methods=['GET'])
-def home():
-    return jsonify(scheduler.get_next_pass().info)
+def next_pass():
+    try:
+        if request.args.get("after") != None:
+            return jsonify(scheduler.get_next_pass(int(request.args.get("after"))).info)
+        else:
+            return jsonify(scheduler.get_next_pass().info)
+    except Exception as e:
+        utils.log(e)
+        return str(e), 400
 
 
 if __name__ == "__main__":
